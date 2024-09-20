04e4cf9100b084a25efbd838a00814a83b173438
Palheiro1 <ugioc@riseup.net>
2024-09-20 19:33:29 +0200

    Final
    Muitas melhoras de aparência e jogabilidade.
    Projeto finalizado.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'; // CSS atualizado
 import coche from './carro.jpeg'; // Importa a imagem do carro
 import cabra from './cabra.png'; // Importa a imagem da cabra
-
+import door from './door-image.png';
 
 // Componente para representar uma porta
-const Door = ({ number, onClick, isSelected, isDisabled, isOpen, isDimmed, showButton, buttonText, onButtonClick, image }) => {
-  const doorClass = `door ${isSelected ? 'door-selected' : ''} ${isDimmed ? 'door-dimmed' : ''}`;
+const Door = ({ number, onClick, isSelected, isDisabled, isOpen, isDimmed, image, isFading, isHidden, isFinal }) => {
+  if (isHidden) {
+    return null; // Não renderiza a porta se estiver oculta
+  }
 
+  const doorClass = `door ${isSelected ? 'door-selected' : ''} ${isDimmed ? 'door-dimmed' : ''} ${isFading ? 'door-fading' : ''}`;
+  const containerClass = `door-container ${isFinal ? 'door-final' : ''}`;
+  
 
   return (
-    <div className="door-container">
+    <div className={containerClass}>
       <div className={`door-frame ${isOpen ? 'door-revealed' : ''}`}>
         {isOpen ? (
           <div className="door-open">
@@ -18,11 +23,8 @@ const Door = ({ number, onClick, isSelected, isDisabled, isOpen, isDimmed, showB
           </div>
         ) : (
           <div className={doorClass} onClick={isDisabled ? null : onClick}>
-            {!isOpen && showButton && (
-              <button className="choice-button" onClick={onButtonClick}>
-                {buttonText}
-              </button>
-            )}
+            <img src={door} alt="Porta" className="door-image" />
+            <div className="door-number">{number}</div>
           </div>
         )}
       </div>
@@ -30,147 +32,178 @@ const Door = ({ number, onClick, isSelected, isDisabled, isOpen, isDimmed, showB
   );
 };
 
-
-// Função principal da aplicação
+// Função principal do aplicativo
 function App() {
-  // Estado inicial para o índice da porta que contém o prêmio
   const [tesouroIndex, setTesouroIndex] = useState(Math.floor(Math.random() * 3));
-  // Estado para a porta selecionada pelo usuário
   const [portaSelecionada, setPortaSelecionada] = useState(null);
-  // Estado que indica se o jogo acabou
   const [jogoAcabou, setJogoAcabou] = useState(false);
-  // Estado que indica se todas as portas foram reveladas
   const [portasReveladas, setPortasReveladas] = useState(false);
-  // Estado para a porta alternativa oferecida pela aplicação
-  const [portaAlternativa, setPortaAlternativa] = useState(null);
-  // Estado que controla se o usuário pode trocar a escolha
   const [podeTrocar, setPodeTrocar] = useState(false);
-  // Contagem de vitórias
   const [contagemVitorias, setContagemVitorias] = useState(0);
-  // Contagem de derrotas
   const [contagemDerrotas, setContagemDerrotas] = useState(0);
-  // Estado para a escolha final do usuário
   const [escolhaFinal, setEscolhaFinal] = useState(null);
-  // Estado para a porta revelada pela aplicação
   const [portaRevelada, setPortaRevelada] = useState(null);
-  // Estado para saber se a porta foi aberta
-  const [portaAberta, setPortaAberta] = useState(false);
-  // Estado para a porta final selecionada
   const [portaSelecionadaFinal, setPortaSelecionadaFinal] = useState(null);
-  // Novo estado para a porta descartada
+  const [portaAlternativa, setPortaAlternativa] = useState(null);
   const [portaDescartada, setPortaDescartada] = useState(null);
+  const [portaReveladaAberta, setPortaReveladaAberta] = useState(false);
+  const [portaDesvanecendo, setPortaDesvanecendo] = useState(false);
+  const [portaOculta, setPortaOculta] = useState(false);
 
-
-  // Calcula a porcentagem de vitórias
   const totalJogos = contagemVitorias + contagemDerrotas;
   const porcentagemVitorias = totalJogos > 0 ? ((contagemVitorias / totalJogos) * 100).toFixed(2) : 0;
+  const allDoors = [...Array(3).keys()];
 
-
-  // Função chamada ao clicar em uma porta
   const handleDoorClick = (index) => {
     if (!jogoAcabou && portaSelecionada === null) {
       setPortaSelecionada(index);
-
-
-      if (index === tesouroIndex) {
-        // Se a porta selecionada contém o prêmio, revela uma porta aleatória que não foi escolhida
-        const outrasPortas = [...Array(3).keys()].filter(i => i !== index);
-        const portaAleatoria = outrasPortas[Math.floor(Math.random() * outrasPortas.length)];
-        setPortaAlternativa(portaAleatoria);
-        setPortaRevelada(outrasPortas.find(i => i !== portaAleatoria));
-        setPortaAberta(true);
-      } else {
-        // Se a porta selecionada não contém o prêmio, revela a porta que não foi escolhida e não é a porta com o prêmio
-        setPortaAlternativa(tesouroIndex);
-        setPortaRevelada([...Array(3).keys()].find(i => i !== index && i !== tesouroIndex));
-        setPortaAberta(true);
+      console.log(`Tesouro está na porta: ${tesouroIndex}`);
+    
+      const outrasPortas = [...Array(3).keys()].filter(i => i !== index);
+  
+      // Verifica se sempre há duas outras portas
+      if (outrasPortas.length !== 2) {
+        console.error("Erro: o número de outras portas não é 2.");
+        return;
       }
-
-
-      setPodeTrocar(true);
+  
+      let portaAlternativa;
+      
+      if (index === tesouroIndex) {
+        // O jogador escolheu o tesouro, selecionamos uma cabra aleatoriamente
+        portaAlternativa = outrasPortas[Math.floor(Math.random() * outrasPortas.length)];
+      } else {
+        // O jogador escolheu uma cabra, portaAlternativa é o tesouro
+        portaAlternativa = outrasPortas.find(i => i === tesouroIndex);
+      }
+  
+      // Depuração: verifica portaAlternativa
+      if (portaAlternativa === null || portaAlternativa === undefined) {
+        console.error("Erro: portaAlternativa é null ou undefined.");
+        return;
+      }
+    
+      setPortaRevelada(outrasPortas.find(i => i !== portaAlternativa)); // Revelamos a porta que não é a alternativa
+      setPortaAlternativa(portaAlternativa); // Guardamos a portaAlternativa
+      setPodeTrocar(true); // Permitir trocar
     }
   };
+  
+  
+  useEffect(() => {
+    if (portaRevelada !== null) {
+      setPortaDesvanecendo(true); // Inicia o desvanecimento
+      
+      const timer = setTimeout(() => {
+        setPortaOculta(true); // Oculta o contêiner depois de 2 segundos
+      }, 1500);
+      
+      return () => clearTimeout(timer); // Limpa o temporizador se o componente for desmontado
+    }
+  }, [portaRevelada]);
 
-
-  // Função chamada para confirmar a decisão do usuário (trocar ou manter a escolha)
   const handleDecision = (trocar) => {
-    const escolha = trocar ? portaAlternativa : portaSelecionada;
-    setEscolhaFinal(escolha);
-    setPortaSelecionadaFinal(escolha); // Estabelece a porta selecionada final
-    setPortaDescartada(trocar ? portaSelecionada : portaAlternativa); // Define a porta descartada
-    setJogoAcabou(true);
-
-
-    if (escolha === tesouroIndex) {
-      setContagemVitorias(contagemVitorias + 1);
-    } else {
-      setContagemDerrotas(contagemDerrotas + 1);
+    if (portaSelecionada === null || portaAlternativa === null) {
+      console.error('Erro: portaSelecionada ou portaAlternativa são nulos.');
+      console.log(portaSelecionada);
+      console.log(portaAlternativa);
+      return;
     }
-
-
-    // Revela todas as portas no final do jogo
+  
+    const novaEscolha = trocar ? portaAlternativa : portaSelecionada;
+    
+    setPortaSelecionadaFinal(novaEscolha);
+    setPortaSelecionada(novaEscolha);
+    setPortaDescartada(trocar ? portaSelecionada : portaAlternativa);
+    setJogoAcabou(true);
     setPortasReveladas(true);
+    setEscolhaFinal(novaEscolha);
+  
+    // Depuração
+    console.log(`Tesouro está na porta: ${tesouroIndex}`);
+    console.log(`Escolha final: ${novaEscolha}`);
+  
+    setTimeout(() => {
+      if (novaEscolha === tesouroIndex) {
+        console.log("Vitória!");
+        setContagemVitorias(prev => prev + 1);
+      } else {
+        console.log("Derrota!");
+        setContagemDerrotas(prev => prev + 1);
+      }
+    }, 200); 
   };
+  
+  
+  
+  
 
-
-  // Função para reiniciar o jogo
   const resetGame = () => {
     setPortaSelecionada(null);
-    setPortaSelecionadaFinal(null); // Reinicia a porta selecionada final
+    setPortaSelecionadaFinal(null);
     setJogoAcabou(false);
     setPortasReveladas(false);
     setPortaAlternativa(null);
     setPodeTrocar(false);
-    setTesouroIndex(Math.floor(Math.random() * 3));
     setEscolhaFinal(null);
     setPortaRevelada(null);
-    setPortaAberta(false);
-    setPortaDescartada(null); // Reinicia a porta descartada
+    setPortaDescartada(null);
+    setPortaOculta(false);
+    
+    // Certifique-se de que o tesouro só seja atualizado ao reiniciar
+    setTimeout(() => {
+      setTesouroIndex(Math.floor(Math.random() * 3));
+    }, 100);
   };
-
+  
 
   return (
     <div className="app">
       <h1>Jogo das 3 Portas</h1>
-      <h2>Detrás de umha destas portas há um esplêndido carro, e detrás das outras duas... um par de cabras.</h2>
-      <h2>Escolhe umha, e das restantes duas, vamos descartar umha que contenha umha cabra. Entom poderás mudar a tua aposta final, se queres.</h2>
+      <h4>Detrás de uma destas portas há um esplêndido carro, e detrás das outras duas... um par de cabras.</h4>
+      <h4>Escolha uma, e das restantes duas, vamos descartar uma que contenha uma cabra. Depois poderás mudar a tua aposta final, se quiseres.</h4>
+
       <div className="doors">
-        {[...Array(3)].map((_, index) => (
+        {allDoors.map((_, index) => (
           <Door
             key={index}
             number={index + 1}
             onClick={() => handleDoorClick(index)}
-            isSelected={index === portaSelecionada || index === portaSelecionadaFinal} // Marca a porta selecionada final
-            isOpen={portasReveladas && (index === tesouroIndex || index === portaRevelada) || (index === portaRevelada && portaAberta)}
+            isSelected={index === portaSelecionada || index === escolhaFinal}
+            isOpen={index === portaRevelada || jogoAcabou && (index === 0 || index === 1 || index === 2)}
+            isFinal={index === escolhaFinal}  
             isDisabled={jogoAcabou || podeTrocar}
-            isDimmed={podeTrocar && index !== portaSelecionada && index !== portaAlternativa && index !== portaDescartada} // Desativa a porta descartada
-            showButton={podeTrocar && (index === portaSelecionada || index === portaAlternativa)}
-            buttonText={index === portaSelecionada ? "Manter escolha" : "Trocar de porta"}
-            onButtonClick={() => handleDecision(index === portaAlternativa)}
-            image={index === tesouroIndex ? coche : (index === portaRevelada || index === portaDescartada ? cabra : cabra)} // Exibe a imagem do carro ou da cabra conforme o estado
+            isDimmed={podeTrocar && index !== portaSelecionadaFinal && index !== portaAlternativa && index !== portaDescartada}
+            isFading={index === portaRevelada && portaDesvanecendo}
+            isHidden={index === portaRevelada && portaOculta}
+            image={index === tesouroIndex ? coche : cabra}
           />
         ))}
       </div>
 
-
-      {jogoAcabou && (
-        <div className="result">
-          {escolhaFinal === tesouroIndex ? 'Parabéns, ganhache o carro!' : 'Conforma-te com umha cabra...'}
+      {!jogoAcabou && podeTrocar && (
+        <div className="decision-container">
+          <h3>Agora que só restam duas portas... queres mudar de ideia?</h3>
+          <button className="choice-button" onClick={() => handleDecision(false)}>Manter escolha</button>
+          <button className="choice-button" onClick={() => handleDecision(true)}>Trocar de porta</button>
         </div>
       )}
 
-
       {jogoAcabou && (
-        <button className="reset-button" onClick={resetGame}>
-          Jogar novamente
-        </button>
+        <div className="result">
+          {escolhaFinal === tesouroIndex ? 'Parabéns, ganhaste o carro!' : 'Conforma-te com uma cabra...'}
+
+        </div>
       )}
 
+      {jogoAcabou && (
+        <button className="reset-button" onClick={resetGame}>Jogar novamente</button>
+      )}
 
       <div className="scoreboard">
-        <p>Partidas jogadas: {contagemVitorias + contagemDerrotas}</p>
         <p>Vitórias: {contagemVitorias}</p>
-        <p>Porcentagem de Vitórias: {porcentagemVitorias}%</p>
+        <p>Derrotas: {contagemDerrotas}</p>
+        <p>Porcentagem de vitórias: {porcentagemVitorias}%</p>
       </div>
       <div className="footer">
         <p className="footer-text">
@@ -178,13 +211,12 @@ function App() {
         </p>
         <p className="footer-text">
           <strong>Resultados esperados:</strong><br />
-          Se  <strong> sempre mantés a porta</strong>, ganharás <strong>33%</strong> das vezes.<br />
-          Se  <strong> sempre trocas a porta</strong>, ganharás <strong>67%</strong> das vezes.
+          Se <strong>sempre manténs a porta</strong>, ganharás <strong>33%</strong> das vezes.<br />
+          Se <strong>sempre trocas a porta</strong>, ganharás <strong>66%</strong> das vezes.
         </p>
       </div>
     </div>
   );
 }
-
 
 export default App;
